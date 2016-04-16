b10d8f47fe395bbefa8116d7284350945e87389e
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-04-16 10:53:30 -0700

    Correct errors revealed through testing

--- assets/js/course.js
+++ assets/js/course.js
@@ -1,32 +1,30 @@
-(function() {
-  'use strict';
+'use strict';
 
-  function Course(courseJson) {
-    var courseSplit = courseJson.course.split(' ', 2);
-    this.department = courseSplit[0];
-    this.courseNumber = courseSplit[1];
+function Course(courseJson) {
+  var courseSplit = courseJson.course.split(' ', 2);
+  this.department = courseSplit[0];
+  this.courseNumber = courseSplit[1];
 
-    this.title = courseJson.title;
-    this.instructor = courseJson.instructor;
-    this.ccn = courseJson.ccn;
-    this.units = courseJson.units;
-    this.sections = courseJson.sections;
+  this.title = courseJson.title;
+  this.instructor = courseJson.instructor;
+  this.ccn = courseJson.ccn;
+  this.units = courseJson.units;
+  this.sections = courseJson.sections;
 
-    this.sectionTypes = [];
-    this.sections.forEach(function(section) {
-      if (this.sectionTypes.indexOf(section.type) < 0) {
-        this.sectionTypes.push(section.type);
-      }
-    }, this);
-  }
+  this.sectionTypes = [];
+  this.sections.forEach(function(section) {
+    if (this.sectionTypes.indexOf(section.type) < 0) {
+      this.sectionTypes.push(section.type);
+    }
+  }, this);
+}
 
-  Course.parse = function(data) {
-    return new Course(data);
-  };
+Course.parse = function(data) {
+  return new Course(data);
+};
 
-  Course.prototype.getSectionsByType = function(type) {
-    return this.sections.filter(function(section) {
-      return section.type === type;
-    });
-  }
-})();
+Course.prototype.getSectionsByType = function(type) {
+  return this.sections.filter(function(section) {
+    return section.type === type;
+  });
+};
