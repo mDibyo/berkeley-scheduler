d391d934274962be927872a6c380fcf34023bf58
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-04-17 19:00:10 -0700

    Refactor models into SEAFs

--- assets/js/course.js
+++ assets/js/course.js
@@ -1,36 +1,89 @@
 'use strict';
 
-function Course(courseJson) {
-  var courseSplit = courseJson.course.split(' ', 2);
-  this.department = courseSplit[0];
-  this.courseNumber = courseSplit[1];
+var Course = (function() {
+  function Course(courseJson) {
+    var courseSplit = courseJson.course.split(' ', 2);
+    this.department = courseSplit[0];
+    this.courseNumber = courseSplit[1];
 
-  this.title = courseJson.title;
-  this.instructor = courseJson.instructor;
-  this.ccn = courseJson.ccn;
-  this.units = courseJson.units;
+    this.title = courseJson.title;
+    this.instructor = courseJson.instructor;
+    this.ccn = courseJson.ccn;
+    this.units = courseJson.units;
 
-  this.sections = courseJson.sections;
-  this.sectionTypes = [];
-  this.sections.forEach(function(section) {
-    section.course = this;
-    section.selected = true;
+    this.sections = courseJson.sections;
+    this.sectionTypes = [];
+    this.sections.forEach(function(section) {
+      section.course = this;
+      section.selected = true;
 
-    if (this.sectionTypes.indexOf(section.type) < 0) {
-      this.sectionTypes.push(section.type);
+      if (this.sectionTypes.indexOf(section.type) < 0) {
+        this.sectionTypes.push(section.type);
+      }
+    }, this);
+
+    this.color = null;
+    this.view = false;
+  }
+
+  Course.colorCodes = {
+    'red': '#f44336',
+    'pink': '#e91e63',
+    'purple': '#9c27b0',
+    'deep-purple': '3673ab7',
+    'indigo': '#3f51b5',
+    'blue': '#2196f3',
+    'light-blue': '303a9f4',
+    'cyan': '#00bcd4',
+    'teal': '#009688',
+    'green': '34caf50',
+    'light-green': '#8bc34a',
+    'lime': '#cddc39',
+    'yellow': '#ffeb3b',
+    'amber': '#ffc107',
+    'orange': '#ff9800',
+    'deep-orange': '#ff5722',
+    'brown': '#795548',
+    'blue-grey': '#607d8b'
+  };
+
+  Course.unregisteredColors = Object.keys(Course.colorCodes);
+
+  Course.registeredColors = [];
+
+  Course.getRegisteredColor = function() {
+    var l = Course.unregisteredColors.length;
+    if (l == 0) {
+      return Course.registeredColors[Math.floor(Math.random() * Course.registeredColors.length)]
     }
-  }, this);
+    var idx = Math.floor(Math.random() * l);
+    var color = Course.unregisteredColors.splice(idx, 1)[0];
+    Course.registeredColors.push(color);
+    return color;
+  };
 
-  this.color = null;
-  this.view = false;
-}
+  Course.unRegisterColor = function(color) {
+    Course.registeredColors.remove(color);
+    Course.unregisteredColors.push(color);
+  };
 
-Course.parse = function(data) {
-  return new Course(data);
-};
+  Course.parse = function(data) {
+    return new Course(data);
+  };
 
-Course.prototype.getSectionsByType = function(type) {
-  return this.sections.filter(function(section) {
-    return section.type === type;
-  });
-};
+  Course.prototype.getSectionsByType = function(type) {
+    return this.sections.filter(function(section) {
+      return section.type === type;
+    });
+  };
+
+  Course.prototype.add = function() {
+    this.color = Course.getRegisteredColor();
+  };
+
+  Course.prototype.drop = function() {
+    Course.unRegisterColor(this.color);
+  };
+
+  return Course;
+})();
--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,101 +1,105 @@
 'use strict';
 
-function Schedule(sections) {
-  this.courses = {};
-  this.meetingsByDay = {
-    'Monday': [],
-    'Tuesday': [],
-    'Wednesday': [],
-    'Thursday': [],
-    'Friday': []
+var Schedule = (function() {
+  function Schedule(sections) {
+    this.courses = {};
+    this.meetingsByDay = {
+      'Monday': [],
+      'Tuesday': [],
+      'Wednesday': [],
+      'Thursday': [],
+      'Friday': []
+    };
+
+    var ccnList = [];
+    sections.forEach(function (section) {
+      ccnList.push(section.ccn);
+      var ccn = section.course.ccn;
+      if (!this.courses.hasOwnProperty(ccn)) {
+        this.courses[ccn] = [];
+      }
+      this.courses[ccn].push(section);
+
+      var dayAbrvs = section.time.split(' ', 2)[0];
+      for (var i = 0; i < dayAbrvs.length; i++) {
+        this.meetingsByDay[Schedule.dayAbrvExpansions[dayAbrvs[i]]].push(section);
+      }
+    }, this);
+    this.id = Schedule.generateId(ccnList);
+
+    this.selected = true;
+  }
+
+  Schedule.generateId = function(ccnList) {
+    ccnList = ccnList.map(function (ccn) {
+      return parseInt(ccn);
+    });
+    ccnList.sort(function(a, b) {
+      return a - b;
+    });
+    return ccnList.join('.');
   };
 
-  var ccnList = [];
-  sections.forEach(function (section) {
-    ccnList.push(section.ccn);
-    var ccn = section.course.ccn;
-    if (!this.courses.hasOwnProperty(ccn)) {
-      this.courses[ccn] = [];
+  Schedule.normalizeId = function(id) {
+    if (id === null) {
+      return null;
     }
-    this.courses[ccn].push(section);
+    return Schedule.generateId(id.split('.'));
+  };
 
-    var dayAbrvs = section.time.split(' ', 2)[0];
-    for (var i = 0; i < dayAbrvs.length; i++) {
-      this.meetingsByDay[Schedule.dayAbrvExpansions[dayAbrvs[i]]].push(section);
-    }
-  }, this);
-  this.id = Schedule.generateId(ccnList);
+  Schedule.dayAbrvExpansions = {
+    M: 'Monday',
+    T: 'Tuesday',
+    W: 'Wednesday',
+    R: 'Thursday',
+    F: 'Friday'
+  };
 
-  this.selected = true;
-}
+  Schedule.intervalAbrvPositionOffsets = {
+    '8-9A': 0,
+    '8-9:30A': 0,
+    '9-9A': 1,
+    '9:30-11A': 1.5,
+    '10-11A': 2,
+    '11-12P': 3,
+    '11-12:30P': 4,
+    '12-1P': 4,
+    '12:30-2P': 4.5,
+    '1-2P': 5,
+    '2-3P': 6,
+    '2-3:30P': 6,
+    '3-4P': 7,
+    '3:30-5P': 7.5,
+    '4-5P': 8,
+    '5-6P': 9,
+    '5-6:30P': 9,
+    '6-7P': 10,
+    '6:30-8P': 10.5,
+    '7-8P': 11
+  };
 
-Schedule.generateId = function(ccnList) {
-  ccnList = ccnList.map(function (ccn) {
-    return parseInt(ccn);
-  });
-  ccnList.sort(function(a, b) {
-    return a - b;
-  });
-  return ccnList.join('.');
-};
+  Schedule.intervalAbrvHeights = {
+    '8-9A': 1,
+    '8-9:30A': 1.5,
+    '9-9A': 1,
+    '9:30-11A': 1.5,
+    '10-11A': 1,
+    '11-12P': 1,
+    '11-12:30P': 1.5,
+    '12-1P': 1,
+    '12:30-2P': 1.5,
+    '1-2P': 1,
+    '2-3P': 1,
+    '2-3:30P': 1.5,
+    '3-4P': 1,
+    '3:30-5P': 1.5,
+    '4-5P': 1,
+    '5-6P': 1,
+    '5-6:30P': 1.5,
+    '6-7P': 1,
+    '6:30-8P': 1.5,
+    '7-8P': 1
+  };
 
-Schedule.normalizeId = function(id) {
-  if (id === null) {
-    return null;
-  }
-  return Schedule.generateId(id.split('.'));
-};
-
-Schedule.dayAbrvExpansions = {
-  M: 'Monday',
-  T: 'Tuesday',
-  W: 'Wednesday',
-  R: 'Thursday',
-  F: 'Friday'
-};
-
-Schedule.intervalAbrvPositionOffsets = {
-  '8-9A': 0,
-  '8-9:30A': 0,
-  '9-9A': 1,
-  '9:30-11A': 1.5,
-  '10-11A': 2,
-  '11-12P': 3,
-  '11-12:30P': 4,
-  '12-1P': 4,
-  '12:30-2P': 4.5,
-  '1-2P': 5,
-  '2-3P': 6,
-  '2-3:30P': 6,
-  '3-4P': 7,
-  '3:30-5P': 7.5,
-  '4-5P': 8,
-  '5-6P': 9,
-  '5-6:30P': 9,
-  '6-7P': 10,
-  '6:30-8P': 10.5,
-  '7-8P': 11
-};
-
-Schedule.intervalAbrvHeights = {
-  '8-9A': 1,
-  '8-9:30A': 1.5,
-  '9-9A': 1,
-  '9:30-11A': 1.5,
-  '10-11A': 1,
-  '11-12P': 1,
-  '11-12:30P': 1.5,
-  '12-1P': 1,
-  '12:30-2P': 1.5,
-  '1-2P': 1,
-  '2-3P': 1,
-  '2-3:30P': 1.5,
-  '3-4P': 1,
-  '3:30-5P': 1.5,
-  '4-5P': 1,
-  '5-6P': 1,
-  '5-6:30P': 1.5,
-  '6-7P': 1,
-  '6:30-8P': 1.5,
-  '7-8P': 1
-};
+  return Schedule;
+})();
