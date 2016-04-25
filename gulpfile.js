15cb3af3f74025bd00a19c67a37601c9b096a0a4
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-04-25 16:19:44 -0700

    Add dependencies as node modules

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  var gulp = require('gulp');
+
+  var paths = {
+    self: 'gulpfile.js',
+    src: {
+      angular: 'js/src/angular/app.js',
+      lib: 'js/src/lib/index.js'
+    }
+  }
+});
---
+++ js/src/lib/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('angular');
+require('angular-aria');
+//require('angular-resource');
+require('angular-animate');
+require('angular-cookies');
+require('angular-material');
+require('angular-sanitize');
+require('angular-ui-router');
