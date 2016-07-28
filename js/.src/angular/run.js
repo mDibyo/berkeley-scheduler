c1f982203de8b2500087450d695f6e29aaced3f4
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-07-28 00:35:33 -0700

    Fix layout of main elements in app

--- js/.src/angular/run.js
+++ js/.src/angular/run.js
@@ -1,11 +1,40 @@
 angular.module('scheduleBuilder').run([
+  '$window',
   '$state',
   '$rootScope',
   '$mdDialog',
   '$mdMedia',
   'scheduleFactory',
-  function($state, $rootScope, $mdDialog, $mdMedia, scheduleFactory) {
+  function($window, $state, $rootScope, $mdDialog, $mdMedia, scheduleFactory) {
     $rootScope.$state = $state;
+
+    var bodyHeight = null;
+    var leftPane = null;
+    var leftPaneHeight = null;
+    var rightPane = null;
+
+    function getHeight(element) {
+      return Math.max(element.offsetHeight, element.clientHeight);
+    }
+
+    function setRightPaneHeight() {
+      if (leftPane && rightPane) {
+        bodyHeight = getHeight(document.body);
+        leftPaneHeight = getHeight(leftPane);
+        if (leftPaneHeight > bodyHeight) {
+          rightPane.style['min-height'] = leftPaneHeight;
+        } else {
+          rightPane.style['min-height'] = null;
+        }
+      }
+    }
+
+    $rootScope.$on('$viewContentLoaded', function() {
+      leftPane = leftPane || document.getElementById('left-pane');
+      rightPane = rightPane || document.getElementById('right-pane');
+      setRightPaneHeight();
+    });
+    $window.addEventListener('resize', setRightPaneHeight);
 
     var showMobUnoptDialog =
       scheduleFactory.getPreferences()['showMobUnoptDialog'];
