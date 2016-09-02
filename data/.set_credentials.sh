a50e4ce18eab1e7e3675ed005fb1bda9396de752
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-09-01 20:20:37 -0700

    Add sample .set_credentials file

---
+++ data/.set_credentials.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# SIS Course API (https://api-central.berkeley.edu/api/46)
+export SIS_COURSE_API_APP_ID=replaceme
+export SIS_COURSE_API_APP_KEY=replaceme
+
+# SIS Class API (https://api-central.berkeley.edu/api/45)
+export SIS_CLASS_API_APP_ID=replaceme
+export SIS_CLASS_API_APP_KEY=replaceme
