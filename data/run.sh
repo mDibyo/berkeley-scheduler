49ec5693b525dd54fffc10496e281268818c4f32
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-10-05 19:56:31 -0700

    Add Spring 2016 Class API data - 10/5

--- data/run.sh
+++ data/run.sh
@@ -2,6 +2,8 @@
 
 source .set_credentials.sh
 
+term='spring-2017'
+
 daily=true
 
 # Fetch course info
@@ -10,6 +12,7 @@ ${daily} || ./.scripts/extract_course_json.py || exit $?
 
 # Extract into departments
 ${daily} || ./.scripts/extract_departments_from_course_json.py || exit $?
+${daily} || cp -r intermediate/departments/course-listing-by-subject-area intermediate/departments/class-listing-${term}-by-subject-area
 
 # Fetch classes for a term
 ./.scripts/fetch_and_extract_class_json.py || exit $?
