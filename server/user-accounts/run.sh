a4667cf4e2cc685d381e9c4c5d4f5ac67a2efc0f
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-11-18 00:16:58 -0800

    Update user-accounts docker container

--- server/user-accounts/run.sh
+++ server/user-accounts/run.sh
@@ -2,4 +2,10 @@
 
 : ${APP_ROOT:=/berkeley-scheduler}
 
-python3 ${APP_ROOT}/server/user-accounts/src/server.py 80
+echo 'HERE AGAIN!'
+
+python3 -m http.server --bind 0.0.0.0 8085
+
+# python3 ${APP_ROOT}/server/user-accounts/src/server.py 8080
+
+echo 'THERE!'
