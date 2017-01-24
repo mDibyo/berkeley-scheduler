427f27728368c80d33976f0f93439fa33f4e59ba
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-01-24 13:26:11 -0800

    Add berkeley-warn palette for theme

--- client/js/angular/config.js
+++ client/js/angular/config.js
@@ -48,45 +48,62 @@ angular.module('berkeleyScheduler').config([
 
     $urlRouterProvider.otherwise(scheduleUrl);
 
-    var berkeleyPrimaryPalette = {
-      '50': '#0073e1',
-      '100': '#0066c8',
-      '200': '#0059ae',
-      '300': '#004c95',
-      '400': '#003f7b',
-      '500': '#003262',
-      '600': '#002548',
-      '700': '#00182f',
-      '800': '#000b15',
-      '900': '#000000',
-      'A100': '#0080fb',
-      'A200': '#158dff',
-      'A400': '#2f99ff',
-      'A700': '#000000'
-    };
+    // Palettes defined using
+    // UC Berkeley brand colors: http://www.berkeley.edu/brand/img/colors/WebColor_accessible_AA_swatches.pdf
+    // Angular Material Color generator: https://angular-md-color.com/#/
     $mdThemingProvider
-      .definePalette('berkeley-primary', berkeleyPrimaryPalette);
-    var berkeleyAccentPalette = {
-      '50': '#785301',
-      '100': '#916501',
-      '200': '#ab7601',
-      '300': '#c48802',
-      '400': '#dd9902',
-      '500': '#f6ab02',
-      '600': '#fdbd2e',
-      '700': '#fdc548',
-      '800': '#fecd61',
-      '900': '#fed57a',
-      'A100': '#fdbd2e',
-      'A200': '#FDB515',
-      'A400': '#f6ab02',
-      'A700': '#fedd93'
-    };
-    $mdThemingProvider
-      .definePalette('berkeley-accent', berkeleyAccentPalette);
+        .definePalette('berkeley-primary', {
+          '50': '#0073e1',
+          '100': '#0066c8',
+          '200': '#0059ae',
+          '300': '#004c95',
+          '400': '#003f7b',
+          '500': '#003262',
+          '600': '#002548',
+          '700': '#00182f',
+          '800': '#000b15',
+          '900': '#000000',
+          'A100': '#0080fb',
+          'A200': '#158dff',
+          'A400': '#2f99ff',
+          'A700': '#000000'
+        })
+        .definePalette('berkeley-accent', {
+          '50': '#785301',
+          '100': '#916501',
+          '200': '#ab7601',
+          '300': '#c48802',
+          '400': '#dd9902',
+          '500': '#f6ab02',
+          '600': '#fdbd2e',
+          '700': '#fdc548',
+          '800': '#fecd61',
+          '900': '#fed57a',
+          'A100': '#fdbd2e',
+          'A200': '#FDB515',
+          'A400': '#f6ab02',
+          'A700': '#fedd93'
+        })
+        .definePalette('berkeley-warn', {
+          '50': '#f7b4a8',
+          '100': '#f59f91',
+          '200': '#f38b79',
+          '300': '#f17762',
+          '400': '#ef624a',
+          '500': '#ED4E33',
+          '600': '#eb3a1c',
+          '700': '#da3013',
+          '800': '#c22b11',
+          '900': '#ab260f',
+          'A100': '#f9c8c0',
+          'A200': '#fbdcd7',
+          'A400': '#fef1ee',
+          'A700': '#93210d'
+        });
     $mdThemingProvider.theme('default')
-      .primaryPalette('berkeley-primary')
-      .accentPalette('berkeley-accent');
+        .primaryPalette('berkeley-primary')
+        .accentPalette('berkeley-accent')
+        .warnPalette('berkeley-warn');
 
     $mdIconProvider
       .defaultViewBoxSize(48)
