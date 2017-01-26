b0124d74750e539a04a69f339f215c539f24c20e
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-01-26 10:10:07 -0800

    Add headers for accepting connections from berkeleyscheduler.com

--- server/user-accounts/src/app.js
+++ server/user-accounts/src/app.js
@@ -27,6 +27,11 @@ mongodb.MongoClient.connect(DATABASE_URL)
 
       // Middleware
       app.use(morgan('dev'));
+      app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', 'https://berkeleyscheduler.com');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+      });
       app.use(bodyParser.urlencoded({ extended: true }));
       app.use(bodyParser.json());
 
