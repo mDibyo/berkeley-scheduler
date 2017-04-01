b46df07b93c3bdfb23eefa8aeaed0f72d891bf0e
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-04-01 01:53:33 -0700

    Simplify BaseCtrl class definition

--- client/ts/angular/_base.controller.ts
+++ client/ts/angular/_base.controller.ts
@@ -3,40 +3,32 @@ import angular = require('angular');
 import IScheduleService = require('./schedule.service');
 
 class BaseCtrl {
-  private _$state: angular.ui.IStateService;
-  private _$window: angular.IWindowService;
-  private _scheduleFactory: IScheduleService;
-
   constructor(
-      $state: angular.ui.IStateService,
-      $window: angular.IWindowService,
-      scheduleFactory: IScheduleService
-  ) {
-    this._$state = $state;
-    this._$window = $window;
-    this._scheduleFactory = scheduleFactory;
-  }
+      private $state: angular.ui.IStateService,
+      private $window: angular.IWindowService,
+      protected scheduleFactory: IScheduleService
+  ) {}
 
   goToState(to: string, params: any, options?: angular.ui.IHrefOptions) {
     if (to === 'schedule.viewSchedule') {
       this._goToScheduleViewSchedule(params, options);
       return;
     }
-    this._$state.go(to, params, options);
+    this.$state.go(to, params, options);
   }
 
   goToExternal(href: string) {
-    this._$window.open(href, '_blank');
+    this.$window.open(href, '_blank');
   }
 
   private _goToScheduleViewSchedule(params: any, options?: angular.ui.IHrefOptions) {
-    const schedulingOptions = this._scheduleFactory.getSchedulingOptions();
+    const schedulingOptions = this.scheduleFactory.getSchedulingOptions();
     params.noTimeConflicts = schedulingOptions.noTimeConflicts;
-    this._$state.go('schedule.viewSchedule', params, options);
+    this.$state.go('schedule.viewSchedule', params, options);
   }
 
   getHref(state: angular.ui.IState, params: any, options: angular.ui.IHrefOptions) {
-    return 'https://berkeleyscheduler.com/' + this._$state.href(state, params, options);
+    return 'https://berkeleyscheduler.com/' + this.$state.href(state, params, options);
   }
 }
 
